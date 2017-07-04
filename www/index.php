f55307b202b088c44f9340a9697bb36c11bc7389
Francisco Miranda <francisco.miranda@docker.com>
2017-07-04 15:27:21 -0700

    update index.php
    Add napoleon :)

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello world!</title>
+	<title>Pull Request Placeholder</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
@@ -16,39 +16,26 @@
 	</style>
 </head>
 <body>
-	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
-	<?php
-	$links = [];
-	foreach($_ENV as $key => $value) {
-		if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
-			$links[] = [
-				"name" => $matches[1],
-				"port" => $matches[2],
-				"proto" => $matches[3],
-				"value" => $value
-			];
-		}
-	}
-	if($links) {
-	?>
-		<h3>Links found</h3>
-		<?php
-		foreach($links as $link) {
-			?>
-			<b><?php echo $link["name"]; ?></b> listening in <?php echo $link["port"]+"/"+$link["proto"]; ?> available at <?php echo $link["value"]; ?><br />
-			<?php
-		}
-		?>
-	<?php
-	}
-
-	if($_ENV["DOCKERCLOUD_AUTH"]) {
-		?>
-		<h3>I have Docker Cloud API powers!</h3>
-		<?php
-	}
-	?>
+	<p>
+		<b><?php echo $_ENV["SERVICE_NAME"] ?></b> will update automatically once we receive the webhook from Docker Hub
+	</p>
+	<p>
+		IMAGE: <b><?php echo $_ENV["TARGET_IMAGE"] ?></b>
+		<br/>
+		GIT SHA: <b><?php echo $_ENV["GIT_SHA"] ?></b>
+		<br/>
+		<a href="<?php echo $_ENV["PR_URL"] ?>"><?php echo $_ENV["PR_TITLE"] ?></a>
+	</p>
+	<p>
+		Meanwhile you can hang out with napoleon dynamite
+		<br />
+		<br />
+		<img id="logo" src="napoleon_dynamite.gif" />
+	</p>
+	<script>
+	setInterval(function(){
+		window.location.reload(true);
+	}, 1000*30);
+	</script>
 </body>
 </html>
